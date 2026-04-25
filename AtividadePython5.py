bf8309b4e88535b3857b48e9caf3b587d2330a62
Lucas Matheus <lucasmatheus135.74@gmail.com>
2026-04-25 12:37:00 -0300

    Atividade Concluida Python 5

---
+++ AtividadePython5.py
@@ -0,0 +1,12 @@
+
+
+def maior_numero(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(maior_numero(10, 25, 18))
