9030a1e5dad77bc3dc9dfe4cb5e1c254903fc328
Lucas Matheus <lucasmatheus135.74@gmail.com>
2026-04-25 12:18:53 -0300

    Tarefa Concluida Python 2

---
+++ AtividadePython2.py
@@ -0,0 +1,12 @@
+
+
+contato = {}
+
+
+contato["nome"] = input("Digite o nome do contato: ")
+contato["telefone"] = input("Digite o telefone do contato: ")
+contato["email"] = input("Digite o email do contato: ")
+
+
+print("\nInformações do contato:")
+print(contato)
