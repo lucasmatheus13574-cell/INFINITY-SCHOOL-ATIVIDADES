843a45d91a6dfbde7a34a4d41a13c43d59bce97c
Lucas Matheus <lucasmatheus135.74@gmail.com>
2026-04-25 12:48:30 -0300

    Tarefa Concluida 7

---
+++ AtividadePython7.py
@@ -0,0 +1,12 @@
+import random
+
+
+def lancar_dados():
+    dado1 = random.randint(1, 6)  
+    dado2 = random.randint(1, 6)  
+    
+    total = dado1 + dado2
+    return total
+
+
+print("Soma dos dados:", lancar_dados())
